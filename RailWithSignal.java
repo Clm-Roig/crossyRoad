182c4c73bfff6aac48d0d533bfa435aeeaa92858
Clément Roig <clm.roig@gmail.com>
2017-04-15 12:56:43 +0200

    Signal en cours (bis)...

--- RailWithSignal.java
+++ RailWithSignal.java
@@ -7,6 +7,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class RailWithSignal extends Rail {
     private final int DELAY_TRAIN = 400;
+    private final int SIGNAL_SIZE = 25;
+    private final int DISTANCE_SIGNAL = 13;
+    
     private int positionTrain;    
     private int delayTrainCounter;
 
@@ -24,24 +27,44 @@ public class RailWithSignal extends Rail {
         }
         
         this.getImage().setColor(new Color(200,200,200));
-        this.getImage().drawOval(10,10,25,25);
-        this.getImage().setColor(new Color(50,200,50));
-        this.getImage().fillOval(10,10,25,25);       
+        this.getImage().drawOval(DISTANCE_SIGNAL,DISTANCE_SIGNAL,SIGNAL_SIZE,SIGNAL_SIZE);  
+        greenSignal();
     }
     
     public void act() {  
+        greenSignal();
         delayTrainCounter++;
-            if(delayTrainCounter >= DELAY_TRAIN) {
-                this.getImage().setColor(new Color(200,50,50));
-                this.getImage().fillOval(10,10,25,25);   
-                addTrain();
-                delayTrainCounter = Greenfoot.getRandomNumber(100);
-            }
         
+        // Controle pour ajout train
+        if(delayTrainCounter >= DELAY_TRAIN) {
+            addTrain();
+            delayTrainCounter = Greenfoot.getRandomNumber(100);
+            
+            // Couleur signal  
+            orangeSignal();
+            redSignal();
+        }               
     }   
     
-    public void addTrain() {
+    private void addTrain() {
         Train train = new Train(super.getDirection());
         getWorld().addObject(train,positionTrain,getY());        
     }  
+        
+    private void greenSignal() {
+        this.getImage().setColor(new Color(50,200,50));
+        this.getImage().fillOval(DISTANCE_SIGNAL,DISTANCE_SIGNAL,SIGNAL_SIZE,SIGNAL_SIZE);    
+    }
+    
+    private void orangeSignal() {
+        this.getImage().setColor(new Color(200,100,20));
+        this.getImage().fillOval(DISTANCE_SIGNAL,DISTANCE_SIGNAL,SIGNAL_SIZE,SIGNAL_SIZE);    
+    }
+    
+    private void redSignal() {
+        this.getImage().setColor(new Color(200,50,50));
+        this.getImage().fillOval(DISTANCE_SIGNAL,DISTANCE_SIGNAL,SIGNAL_SIZE,SIGNAL_SIZE);    
+    }
+    
+    
 }
